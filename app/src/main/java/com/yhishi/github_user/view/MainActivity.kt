8cef75dd53aeb363af47a2a4b6d4418d3de87833
yhishi <ev422119.s.okayama@gmail.com>
2022-04-02 11:35:42 +0900

    MainActivityをview packageに移動

--- app/src/main/java/com/yhishi/github_user/view/MainActivity.kt
+++ app/src/main/java/com/yhishi/github_user/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yhishi.github_user
+package com.yhishi.github_user.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yhishi.github_user.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
