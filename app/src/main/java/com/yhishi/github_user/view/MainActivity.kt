a534358e520b357b7f5f7d16b087670e98c1b588
yhishi <ev422119.s.okayama@gmail.com>
2022-04-02 11:48:55 +0900

    MainActivityからFragmentを呼び出す

--- app/src/main/java/com/yhishi/github_user/view/MainActivity.kt
+++ app/src/main/java/com/yhishi/github_user/view/MainActivity.kt
@@ -2,11 +2,23 @@ package com.yhishi.github_user.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.yhishi.github_user.R
+import com.yhishi.github_user.view.users.UsersFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commit {
+            replace(
+                R.id.container,
+                UsersFragment.newInstance(),
+                UsersFragment::class.java.simpleName
+            )
+        }
     }
 }
