28b296ffb3bef3f853f273b120280fe69ee351b1
yhishi <ev422119.s.okayama@gmail.com>
2022-04-02 12:04:34 +0900

    RetrofitServiceのインスタンスを注入

--- app/src/main/java/com/yhishi/github_user/di/ServiceModule.kt
+++ app/src/main/java/com/yhishi/github_user/di/ServiceModule.kt
@@ -1,12 +1,20 @@
 package com.yhishi.github_user.di
 
-import com.yhishi.github_user.BuildConfig
+import androidx.databinding.ktx.BuildConfig
+import com.squareup.moshi.Moshi
+import com.yhishi.github_user.R
+import com.yhishi.github_user.domain.repository.api.retrofit.RetrofitService
+import com.yhishi.github_user.util.ApplicationResources
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.reactivex.rxjava3.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Named
 import javax.inject.Singleton
@@ -31,4 +39,21 @@ class ServiceModule {
             }
             .build()
     }
+
+    @Singleton
+    @Provides
+    fun providesRetrofitService(
+        resources: ApplicationResources,
+        @Named("Client")
+        client: OkHttpClient,
+    ): RetrofitService {
+        return Retrofit.Builder()
+            .baseUrl(resources.getString(R.string.api_url))
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().build()).asLenient())
+            .client(client)
+            .build()
+            .create(RetrofitService::class.java)
+    }
+
 }
