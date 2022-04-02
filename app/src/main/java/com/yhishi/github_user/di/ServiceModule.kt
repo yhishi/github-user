33a71b970b52376121628a766418c1d693523c4c
yhishi <ev422119.s.okayama@gmail.com>
2022-04-02 10:56:28 +0900

    OkHttpClientのインスタンスを注入

---
+++ app/src/main/java/com/yhishi/github_user/di/ServiceModule.kt
@@ -0,0 +1,34 @@
+package com.yhishi.github_user.di
+
+import com.yhishi.github_user.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ServiceModule {
+    @Named("Client")
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .apply {
+                if (BuildConfig.DEBUG) {
+                    addInterceptor(HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BODY
+                    })
+                }
+            }
+            .build()
+    }
+}
