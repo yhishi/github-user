f0afca6ccff49ee9a2d08ebbd73361e7e03b9297
yhishi <ev422119.s.okayama@gmail.com>
2022-04-02 12:28:34 +0900

    ユーザー一覧用のViewModel作成

---
+++ app/src/main/java/com/yhishi/github_user/viewModel/UsersViewModel.kt
@@ -0,0 +1,38 @@
+package com.yhishi.github_user.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.yhishi.github_user.domain.model.ui.User
+import com.yhishi.github_user.domain.repository.GithubRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class UsersViewModel @Inject constructor(
+    repository: GithubRepository,
+) : ViewModel() {
+
+    private val _users = MutableLiveData<List<User>>()
+
+    val users: LiveData<List<User>> get() = _users
+
+    init {
+        // TODO プログレス表示
+        // TODO 検索文字列をuserNameとして渡す
+        repository.users("hishi")
+            .doOnSuccess { users ->
+                _users.postValue(
+                    users.userInfo.map { user ->
+                        User.of(user)
+                    }
+                )
+            }
+            .doOnError {
+                // TODO エラー処理
+            }
+            .onErrorComplete()
+            .subscribe()
+    }
+}
