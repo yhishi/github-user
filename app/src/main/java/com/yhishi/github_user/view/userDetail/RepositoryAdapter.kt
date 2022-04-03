f754213462d782fea3b04fc27efa6f0185d261eb
yhishi <ev422119.s.okayama@gmail.com>
2022-04-03 17:22:39 +0900

    リポジトリ一覧用のApdaterとViewHolder作成

---
+++ app/src/main/java/com/yhishi/github_user/view/userDetail/RepositoryAdapter.kt
@@ -0,0 +1,46 @@
+package com.yhishi.github_user.view.userDetail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.yhishi.github_user.R
+import com.yhishi.github_user.databinding.ItemRepositoryBinding
+import com.yhishi.github_user.domain.model.ui.Repository
+
+
+/**
+ * リポジトリ一覧のAdapter
+ */
+class RepositoryAdapter : ListAdapter<Repository, RepositoryViewHolder>(DiffCallback) {
+
+    private object DiffCallback : DiffUtil.ItemCallback<Repository>() {
+        override fun areItemsTheSame(
+            oldItem: Repository,
+            newItem: Repository
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: Repository,
+            newItem: Repository
+        ): Boolean = areItemsTheSame(oldItem, newItem)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepositoryViewHolder {
+        val binding = DataBindingUtil.inflate<ItemRepositoryBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.item_repository,
+            parent,
+            false
+        )
+        return RepositoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RepositoryViewHolder, position: Int) {
+        val data = getItem(position)
+        holder.binding.repository = data
+    }
+}
---
+++ app/src/main/java/com/yhishi/github_user/view/userDetail/RepositoryViewHolder.kt
@@ -0,0 +1,10 @@
+package com.yhishi.github_user.view.userDetail
+
+import androidx.recyclerview.widget.RecyclerView
+import com.yhishi.github_user.databinding.ItemRepositoryBinding
+
+
+/**
+ * リポジトリ一覧のセル用ののViewHolder
+ */
+class RepositoryViewHolder(val binding: ItemRepositoryBinding) : RecyclerView.ViewHolder(binding.root)
